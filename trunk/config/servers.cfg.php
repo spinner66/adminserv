b334f7713c89f6527a262d0ac1993a6837c32f2f
kevin.baubet@gmail.com <kevin.baubet@gmail.com@47ccf34e-3d13-3e94-a160-a3c260b1c824>
2012-05-19 14:18:03 +0000

    servers.cfg

---
+++ trunk/config/servers.cfg.php
@@ -0,0 +1,14 @@
+<?php
+class ServerConfig {
+	public static $SERVERS = array(
+		/********************* SERVER CONFIGURATION *********************/
+		
+		'ям Private' => array(
+			'address'		=> 'localhost',
+			'port'			=> 2086,
+			'matchsettings'	=> 'rmp',
+			'adminlevel'	=> array('SuperAdmin' => array('192.168.0.2', '192.168.0.3'), 'Admin' => 'local', 'User' => 'none')
+		),
+	);
+}
+?>
