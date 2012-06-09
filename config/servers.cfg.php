b1466fb0a086c1711596f5efa9309bfa4982ffa0
kevin.baubet <kevin.baubet@gmail.com>
2012-06-09 17:31:38 +0000

    defaut servers.cfg

--- config/servers.cfg.php
+++ config/servers.cfg.php
@@ -3,24 +3,14 @@ class ServerConfig {
 	public static $SERVERS = array(
 		/********************* SERVER CONFIGURATION *********************/
 		
-		'ям Private' => array(
+    /*
+		'new server name' => array(
 			'address'		=> 'localhost',
-			'port'			=> 2086,
-			'matchsettings'	=> 'rmp',
-			'adminlevel'	=> array('SuperAdmin' => array('192.168.0.2', '192.168.0.3'), 'Admin' => 'local', 'User' => 'all')
-		),
-		'ям Relay 1' => array(
-			'address'		=> 'localhost',
-			'port'			=> 2087,
+			'port'			=> 5000,
 			'matchsettings'	=> '',
 			'adminlevel'	=> array('SuperAdmin' => 'all', 'Admin' => 'all', 'User' => 'all')
 		),
-		'ям Relay 2     ' => array(
-			'address'		=> 'localhost',
-			'port'			=> 2088,
-			'matchsettings'	=> '',
-			'adminlevel'	=> array('SuperAdmin' => 'all', 'Admin' => 'all', 'User' => 'all')
-		),
+    */
 	);
 }
 ?>
