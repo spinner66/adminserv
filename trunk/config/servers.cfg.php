d33c961a802e36a0f3e612cb1809ac5dce41f8ec
kevin.baubet@gmail.com <kevin.baubet@gmail.com@47ccf34e-3d13-3e94-a160-a3c260b1c824>
2013-07-20 14:07:56 +0000

    default servers.cfg.php file

--- trunk/config/servers.cfg.php
+++ trunk/config/servers.cfg.php
@@ -3,34 +3,14 @@ class ServerConfig {
 	public static $SERVERS = array(
 		/********************* SERVER CONFIGURATION *********************/
 		
-		'ям Private' => array(
-			'address'		=> 'localhost',
-			'port'			=> 2086,
-			'mapsbasepath'	=> '',
-			'matchsettings'	=> 'rmp',
-			'adminlevel'	=> array('SuperAdmin' => 'all', 'Admin' => 'all', 'User' => 'all')
-		),
-		'ям Shootmania private' => array(
-			'address'		=> 'localhost',
-			'port'			=> 2087,
-			'mapsbasepath'	=> '',
-			'matchsettings'	=> 'rmsm',
-			'adminlevel'	=> array('SuperAdmin' => 'all', 'Admin' => 'all', 'User' => 'none')
-		),
-		'ям Rally-Infernal' => array(
-			'address'		=> 'webtof.org',
-			'port'			=> 2086,
-			'mapsbasepath'	=> '',
-			'matchsettings'	=> 'MatchSettings/rally-infernal',
-			'adminlevel'	=> array('SuperAdmin' => 'all', 'Admin' => 'all', 'User' => 'all')
-		),
-		'ям Shootmania' => array(
-			'address'		=> 'webtof.org',
-			'port'			=> 2087,
-			'mapsbasepath'	=> '',
-			'matchsettings'	=> 'MatchSettings/rm-server1',
-			'adminlevel'	=> array('SuperAdmin' => 'all', 'Admin' => 'all', 'User' => 'none')
-		),
-	);
+    /*
+    'new server name' => array(
+      'address' => 'localhost',
+      'port' => 5000,
+      'mapsbasepath' => '',
+      'matchsettings' => 'MatchSettings/',
+      'adminlevel' => array('SuperAdmin' => 'all', 'Admin' => 'all', 'User' => 'all')
+    ),
+    */
 }
 ?>
